42db93b6d7e7901b642d75f12931543b631e184e
Tim Oelrichs <timoelrichs@gmail.com>
2024-10-13 13:58:21 +0200

    working rest api

---
+++ README.md
@@ -0,0 +1,14 @@
+## DoubleCherryGB GB GTS REST API
+
+<img src="/img/gbgts.png">
+
+**author:** Tim Oelrichs<br> **email:** timoelrichs@gmail.com<br>
+
+## Deno REST API for the DoubleCherryGB GB GTS (in Development)
+
+GB GTS is a online Pokemon Trading Pool for the Game Boy and Game Boy Color
+Pokemon games (Red/Blue/Yellow/Silver/Gold/Chrystal). The frontend will allow it
+to select Pokemons from the online Pool and register them to a spefic TrainerID.
+
+The DoubleCherryGB-libretro core can them load them automatically when you enter
+the ingame Cable Club. Your traded Pokemon will than be added the online pool.
---
+++ controllers/pkm-controller.ts
@@ -0,0 +1,99 @@
+import { Pkm, Checkout } from "../models/pkm-model.ts"
+
+import * as uuid from "jsr:@std/uuid";
+import { Context} from "https://deno.land/x/oak@v17.1.0/mod.ts";
+
+const kv = await Deno.openKv();
+
+export async function getAllPkms(ctx: Context) {
+  const Pkms = [];
+  for await (const res of kv.list<Pkm>({ prefix: ["Pkm"] })) {
+    Pkms.push(res.value);
+  }
+  console.log("GET ALL");
+  ctx.response.body = JSON.stringify(Pkms);
+}
+
+export async function getPkmsBydexNo(ctx: Context) {  
+    const Pkms = [];
+    for await (const res of kv.list<Pkm>({ prefix: ["Pkm"] })) {
+      if(res.value.dexNo == Number(ctx.params.dexNo))
+        Pkms.push(res.value);
+    }
+  ctx.response.body = JSON.stringify(Pkms);
+  }
+
+export async function deletePkmsById(ctx: Context) {
+
+  console.log("DELETE " + ctx.params.id );
+
+  ctx.response.body = await kv.atomic()
+    .delete(["Pkm", ctx.params.id])
+    .commit();
+ 
+}
+
+
+
+
+export async function upsertPkm(ctx: Context) {
+
+  
+  const Pkm: Pkm = await ctx.request.body.json();
+
+    if (Pkm.id == "0") Pkm.id = uuid.v1.generate();
+    const PkmKey = ["Pkm", Pkm.id]; 
+  
+    const oldPkm = await kv.get<Pkm>(PkmKey);
+
+    if (!oldPkm.value) {    
+      const ok = await kv.atomic()
+        .check(oldPkm)
+        .set(PkmKey, Pkm)
+        .commit();
+      if (!ok) ctx.response.status = 500;
+      else ctx.response.body = JSON.stringify(Pkm);
+    } else {
+      const ok = await kv.atomic()
+        .check(oldPkm)
+        .delete(["Pkm_by_dexNo", oldPkm.value.dexNo])
+        .set(PkmKey, Pkm)
+        .commit();
+      if (!ok) ctx.response.status = 500;
+      else ctx.response.body = JSON.stringify(Pkm);
+    }
+  }
+
+export async function PostCheckoutPkms(ctx: Context) {
+
+  const Checkout: Checkout = await ctx.request.body.json();
+
+ console.log("POST CHECKOUT");
+ //TODO DELETE CHECKOUT FROM KV
+
+  const CheckoutKey = ["TrainerId", Checkout.TrainerId];
+
+  const oldCheckout = await kv.get<Checkout>(CheckoutKey);
+
+    const ok = await kv.atomic()
+      .check(oldCheckout)
+      .set(CheckoutKey, Checkout)
+      .commit();
+    if (!ok) ctx.response.status = 500;
+    else ctx.response.body = JSON.stringify(Checkout);
+
+ 
+}
+
+export async function getCheckedOutPkms(ctx: Context) {
+
+  const entry = await kv.get(["TrainerId", ctx.params.trainerId]);
+  if (entry.value) {
+    ctx.response.body = JSON.stringify(entry.value?.Pokemons);
+    console.log("GET: CHECKOUT TrainerId " + ctx.params.trainerId);
+    console.log(entry.value?.Pokemons);
+  }
+  else ctx.response.body = null;
+ 
+}
+
